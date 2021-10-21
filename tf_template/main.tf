7153bc7bc1e57f6c65ab6961ea1d09a4f1a233ff
MACHANDRA11 <chandrakanth.maddana@mediaagility.com>
2021-10-21 18:15:40 +0530

    updated

--- tf_template/main.tf
+++
@@ -1,7 +0,0 @@
-module "gcs_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
-  version = "2.2"
-  project_id  = var.project_id 
-  names = var.names 
-  prefix = var.prefix 
-}
