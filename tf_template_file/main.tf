41aa7232d04597ded7810329a245650750730d76
MACHANDRA11 <chandrakanth.maddana@mediaagility.com>
2021-10-21 21:42:07 +0530

    updated

--- tf_template_file/main.tf
+++ tf_template_file/main.tf
@@ -2,6 +2,6 @@ module "gcs_buckets" {
   source  = "terraform-google-modules/cloud-storage/google"
   version = "2.2"
   project_id  = "gcp_project_id"
-  names = "gcp_bucket_names"
+  names = ["gcp_bucket_names"]
   prefix = "gcp_bucket_prefix"
 }
