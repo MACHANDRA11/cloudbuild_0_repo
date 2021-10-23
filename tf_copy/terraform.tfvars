191b27612ea4bf16389cde700b7ce2f4fcd0cc1d
MACHANDRA11 <chandrakanth.maddana@mediaagility.com>
2021-10-23 06:58:52 +0000

    Updated with build bc8195f8-8313-4ab1-9ffa-31487d34327a from cloudbuild_0_repo commit 69553a8eda64c2568859ebfb6401b6e97307c9d1

---
+++ tf_copy/terraform.tfvars
@@ -0,0 +1,4 @@
+project_id = "devtools-317213"
+names = ["thga112"]
+prefix = "tfg"
+serviceapi = "gcp_service_api.googleapis.com"
---
+++ tf_copy/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  type        = string
+  description = "ID of the project which contains Terraform Service Account"
+}
+
+variable "names" {
+  type        = list(string)
+  description = "bucket names"
+}
+
+variable "prefix" {
+  type        = string
+  description = "bucket prefix"
+}
+
+variable "serviceapi" {
+  type        = string
+  description = "bucket prefix"
+  default =""
+}
