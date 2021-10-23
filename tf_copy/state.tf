294ad8e2a1c449097b1efab026c1bc178279ab7e
MACHANDRA11 <91306369+MACHANDRA11@users.noreply.github.com>
2021-10-23 12:11:26 +0530

    modified

--- tf_copy/state.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "develop-test-tf" #name of backend gcs bucket
-    prefix = "test-tf-template"
-  }
-}
--- tf_template_file/api.tf
+++ tf_template_file/api.tf
@@ -1,11 +1,11 @@
-# resource "google_project_service" "project" {
-#   project = var.project_id
-#   service = var.serviceapi
+resource "google_project_service" "project" {
+  project = var.project_id
+  service = var.serviceapi
 
-#   timeouts {
-#     create = "30m"
-#     update = "40m"
-#   }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
 
-#   disable_dependent_services = true
-# }
+  disable_dependent_services = true
+}
