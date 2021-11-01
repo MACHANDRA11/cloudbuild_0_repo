7cd334f0917e84df1981f9585b86f413e8fe83b3
MACHANDRA11 <91306369+MACHANDRA11@users.noreply.github.com>
2021-11-01 22:03:37 +0530

    updated

--- tf_template_file/main.tf
+++ tf_template_file/main.tf
@@ -7,23 +7,3 @@ module "gcs_buckets" {
 }
 
 
-# resource "google_folder" "department1" {
-#   display_name = var.folder_name
-#   parent       = "organizations/var.org_id"
-# }
-
-# variable "folder_name" {
-#   type        = string
-#   description = "The folder’s display name"
-# }
-
-# variable "org_id" {
-#   type        = number
-#   description = "organization id"
-# }
-
-# folder_name = "gcp_folder_name"
-# org_id = "gcp_org_id"
-
-# sed -i "s/gcp_folder_name/$_FOLDER_NAME_5/g" terraform.tfvars
-# sed -i "s/gcp_org_id/$_ORG_ID_6/g" terraform.tfvars
